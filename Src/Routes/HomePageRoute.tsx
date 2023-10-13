d149898d6bd971b2ff9477b53740443fadd0d7c7
Jiju-avasoft <105510110+jiju-avasoft@users.noreply.github.com>
2023-10-13 16:52:21 +0530

    Configured validation while login (#3)

--- Src/Routes/HomePageRoute.tsx
+++ Src/Routes/HomePageRoute.tsx
@@ -5,6 +5,7 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeScreen from '../Screens/HomeScreen';
 import AccountScreen from '../Screens/AccountScreen';
 import TodoScreen from '../Screens/TodoScreen';
+import {WithLocalSvg} from 'react-native-svg';
 
 const HomePageRoute = () => {
   function SettingsScreen() {
@@ -18,21 +19,90 @@ const HomePageRoute = () => {
   const Tab = createBottomTabNavigator();
 
   return (
-    <Tab.Navigator>
+    <Tab.Navigator screenOptions={{tabBarHideOnKeyboard: true}}>
       <Tab.Screen
         name="Explore"
         component={HomeScreen}
-        options={{headerShown: false}}
+        options={{
+          headerShown: false,
+          tabBarActiveTintColor: '#f25c1c',
+          tabBarInactiveTintColor: '#495bb6',
+          tabBarIcon: ({focused}) => {
+            if (focused) {
+              return (
+                <WithLocalSvg
+                  height={25}
+                  width={25}
+                  asset={require('../Assets/Images/active-explore.svg')}
+                />
+              );
+            } else {
+              return (
+                <WithLocalSvg
+                  height={25}
+                  width={25}
+                  asset={require('../Assets/Images/explore.svg')}
+                />
+              );
+            }
+          },
+        }}
       />
       <Tab.Screen
         name="Todo"
         component={TodoScreen}
-        options={{headerShown: false}}
+        options={{
+          headerShown: false,
+          tabBarActiveTintColor: '#f25c1c',
+          tabBarInactiveTintColor: '#495bb6',
+          tabBarIcon: ({focused}) => {
+            if (focused) {
+              return (
+                <WithLocalSvg
+                  height={25}
+                  width={25}
+                  asset={require('../Assets/Images/active-todo.svg')}
+                />
+              );
+            } else {
+              return (
+                <WithLocalSvg
+                  height={25}
+                  width={25}
+                  asset={require('../Assets/Images/todo.svg')}
+                />
+              );
+            }
+          },
+        }}
       />
       <Tab.Screen
         name="Account"
         component={AccountScreen}
-        options={{headerShown: false}}
+        options={{
+          headerShown: false,
+          tabBarActiveTintColor: '#f25c1c',
+          tabBarInactiveTintColor: '#495bb6',
+          tabBarIcon: ({focused}) => {
+            if (focused) {
+              return (
+                <WithLocalSvg
+                  height={22}
+                  width={22}
+                  asset={require('../Assets/Images/active-account.svg')}
+                />
+              );
+            } else {
+              return (
+                <WithLocalSvg
+                  height={22}
+                  width={22}
+                  asset={require('../Assets/Images/account.svg')}
+                />
+              );
+            }
+          },
+        }}
       />
     </Tab.Navigator>
   );
